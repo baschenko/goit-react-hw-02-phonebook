3be9fd38e3d32383ce6bd63a5962fbd92eea7d4b
Vitaly Baschenko <vitaly.baschenko@gmail.com>
2023-05-30 18:33:35 +0300

    Update ContactForm.styled.jsx

--- src/components/ContactForm/ContactForm.styled.jsx
+++ src/components/ContactForm/ContactForm.styled.jsx
@@ -24,6 +24,7 @@ export const FormInput = styled.input`
 export const FormButton = styled.button`
   padding: 10px 20px;
   border-radius: 5px;
+  cursor: pointer;
   border: none;
   box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.12), 0px 4px 4px rgba(0, 0, 0, 0.06),
     1px 4px 6px rgba(0, 0, 0, 0.16);
